0ebdb26e4b0fd92f39fd44617cc366cdf80e9c5c
antybody <yuanm461@hotmail.com>
2018-12-18 17:28:06 +0800

    Revert "Revert "差分""
    This reverts commit 6e02e6b27b8508b7665cc8bd66c70b2011f8f823.

---
+++ models/wavelet.py
@@ -0,0 +1,64 @@
+#-*-coding:utf-8-*-
+'''
+ 小波分析
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import io as spio
+
+import pandas as pd
+
+'''加载并显示数据'''
+def get_data():
+
+    data = pd.read_excel('../ele.xls',sheet_name='86-2')
+    X = data
+
+
+'''
+  平稳性检验：观察法和单位根检验法 
+'''
+
+from statsmodels.tsa.stattools import adfuller
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+# 移动平均图
+def draw_trend(timeSeries, size):
+    f = plt.figure(facecolor='white')
+    # 对size个数据进行移动平均
+    rol_mean = timeSeries.rolling(window=size).mean()
+    # 对size个数据进行加权移动平均
+    rol_weighted_mean = pd.ewma(timeSeries, span=size)
+
+    timeSeries.plot(color='blue', label='Original')
+    plt.plot(color='red', label='Rolling Mean')
+    rol_weighted_mean.plot(color='black', label='Weighted Rolling Mean')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean')
+    plt.show()
+
+def draw_ts(timeSeries):
+    f = plt.figure(facecolor='white')
+    timeSeries.plot(color='blue')
+    plt.show()
+
+def testStationarity(ts):
+    dftest = adfuller(ts)
+    # 对上述函数求得的值进行语义描述
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic','p-value','#Lags Used','Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    return dfoutput
+
+# 自相关和偏相关图，默认阶数为31阶
+def draw_acf_pacf(ts, lags=31):
+    f = plt.figure(facecolor='white')
+    ax1 = f.add_subplot(211)
+    plot_acf(ts, lags=31, ax=ax1)
+    ax2 = f.add_subplot(212)
+    plot_pacf(ts, lags=31, ax=ax2)
+    plt.show()
