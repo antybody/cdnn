4efc7bfb80f57dfa924bc92639c08f36a4297101
antybody <yuanm461@hotmail.com>
2019-01-11 11:34:30 +0800

    增加拉格朗日插补计算

--- predata/pre_lof.py
+++ predata/pre_lof.py
@@ -8,6 +8,9 @@ import pandas as pd
 import numpy as np
 import impyute as impy
 
+from scipy import interpolate
+from scipy.interpolate import lagrange
+
 def pre_lof(data,year,year1):
     # 设定时间序列
     # data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
@@ -43,6 +46,7 @@ def pre_lof(data,year,year1):
 
 #将0值替换为平缓数据
 def replace_data(df_period):
+    pd.set_option('display.max_rows', None)
     # 替换空值为其他值
     df_period_clone = df_period
     df_period_clone.replace(0, np.nan, inplace=True)
@@ -51,6 +55,9 @@ def replace_data(df_period):
     isnullcon = df_period_clone.isnull().any()
     # print(isnullcon['FP_TOTALENG'])
     # 这里来个判断，如果数据里没有需要平滑的点，那么直接输出
+    df_peroid_v = df_period_clone['dt_val']
+    df_peroid_v.reset_index(drop=True,inplace=True)
+    print(df_peroid_v.index)
     if isnullcon['dt_val']:
         df_after = impy.locf(df_period_clone, axis='dt_val')
         for i in range(len(df_period_clone)):
@@ -60,8 +67,37 @@ def replace_data(df_period):
             print(df_period['dt_val'][i])
 
             df_period_clone['dt_val'][i] = df_after[0][i]
+    # print(df_period_clone)
+    return df_period_clone
+
+# 拉格朗日
+def replace_data_lg(df_period):
+    # pd.set_option('display.max_rows', None)
+    # 替换空值为其他值
+    df_period_clone = df_period
+    df_period_clone.replace(0, np.nan, inplace=True)
+
+    # 平滑后的数据
+    isnullcon = df_period_clone.isnull().any()
+    # print(isnullcon['FP_TOTALENG'])
+    # 这里来个判断，如果数据里没有需要平滑的点，那么直接输出
+    df_peroid_v = df_period_clone['dt_val']
+    df_peroid_v.reset_index(drop=True, inplace=True)
+    print(df_peroid_v.index)
+    if isnullcon['dt_val']:
+        for j in range(len(df_period_clone)):
+            if np.isnan(df_period_clone['dt_val'][j]):
+                df_period_clone['dt_val'][j] = ploy(df_peroid_v, j)
 
     return df_period_clone
+
+# 拉格朗日
+
+def ploy(s,n,k=4):
+    y = s[list(range(n - k, n)) + list(range(n + 1, n + 1 + k))]  # 取数
+    y = y[y.notnull()]
+    out = lagrange(y.index, list(y))(n)
+    return round(out,2)
 
 #将负值替换为0
 def replace_zero(data):
