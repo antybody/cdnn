3db80a1a262bb7f66e10edd1000e4e345b0ddff1
antybody <yuanm461@hotmail.com>
2019-01-14 17:30:14 +0800

    修正算法

--- predata/pre_lof.py
+++ predata/pre_lof.py
@@ -10,6 +10,7 @@ import impyute as impy
 
 from scipy import interpolate
 from scipy.interpolate import lagrange
+from scipy.interpolate import interp1d
 
 def pre_lof(data,year,year1):
     # 设定时间序列
@@ -75,38 +76,42 @@ def replace_data_lg(df_period):
     # pd.set_option('display.max_rows', None)
     # 替换空值为其他值
     df_period_clone = df_period
-    df_period_clone.replace(0, np.nan, inplace=True)
+    df_period_clone['dt_eidt'].replace(0, np.nan, inplace=True)
 
     # 平滑后的数据
     isnullcon = df_period_clone.isnull().any()
     # print(isnullcon['FP_TOTALENG'])
     # 这里来个判断，如果数据里没有需要平滑的点，那么直接输出
-    df_peroid_v = df_period_clone['dt_val']
+    df_peroid_v = df_period_clone['dt_eidt']
     df_peroid_v.reset_index(drop=True, inplace=True)
-    print(df_peroid_v.index)
-    if isnullcon['dt_val']:
+    df_peroid_v_clone = df_peroid_v.copy()
+    inflex =  find_inflexion(df_peroid_v_clone)
+    # print(df_peroid_v.index)
+    if isnullcon['dt_eidt']:
         for j in range(len(df_period_clone)):
-            if np.isnan(df_period_clone['dt_val'][j]):
-                df_period_clone['dt_val'][j] = ploy(df_peroid_v, j)
+            if np.isnan(df_period_clone['dt_eidt'][j]):
+                if abs(j - inflex ) > 3 or inflex == 0:
+                    df_period_clone['dt_eidt'][j] = ploy(df_peroid_v, j)
+                else:
+                    df_period_clone['dt_eidt'][j] = cal_inflexion_val(df_peroid_v_clone,j)
 
     return df_period_clone
 
 # 拉格朗日
-
-def ploy(s,n,k=4):
+def ploy(s,n,k=3):
     y = s[list(range(n - k, n)) + list(range(n + 1, n + 1 + k))]  # 取数
     y = y[y.notnull()]
     out = lagrange(y.index, list(y))(n)
     return round(out,2)
 
-#将负值替换为0
-def replace_zero(data):
+#将指定列值替换为0
+def replace_zero(data,field):
     # data = datas.copy()
     data=data.set_index('dt_time')
     for i in range(len(data)):
-        aaa=data['dt_val'][i]
-        if (data['dt_val'][i] < 0):
-            print(data['dt_val'][i])
+        aaa=data[field][i]
+        if (data[field][i] < 0):
+            print(data[field][i])
     return data
 
 # 拉格朗日1
@@ -129,3 +134,56 @@ def replace_data_lg_max(df_period):
                 df_period_clone['dt_eidt'][j] = ploy(df_peroid_v, j)
 
     return df_period_clone
+
+# 样条插值
+def replace_date_slin(df_period):
+    # 替换空值为其他值
+    df_period_clone = df_period
+    df_period_clone.replace(0, np.nan, inplace=True)
+
+    # 平滑后的数据
+    isnullcon = df_period_clone.isnull().any()
+    # print(isnullcon['FP_TOTALENG'])
+    # 这里来个判断，如果数据里没有需要平滑的点，那么直接输出
+    df_peroid_v = df_period_clone['dt_val']
+    df_peroid_v.reset_index(drop=True, inplace=True)
+    df_peroid_v.dropna(axis=0, how='any')
+    data_allindex = df_peroid_v.index.tolist()
+    fun = interp1d(data_allindex, df_peroid_v, kind= 'zero')
+    if isnullcon['dt_val']:
+        for j in range(len(df_period_clone)):
+            if np.isnan(df_period_clone['dt_val'][j]):
+                df_period_clone['dt_val'][j] = fun(j)
+
+    return df_period_clone
+
+# 找出拐点
+'''
+ 1、 如果拐点所在的序号 < 4 那么就不算有拐点出现
+ 2、 获取拐点所在的序列号，循环 数据时判断 序号是否在其左右，如果在的话，那么就不采用拉格朗日计算方法 
+ 3、 根据 拐点的数值 减去 众值 作为 最后的结果
+'''
+def find_inflexion(df):
+    df_clone = df.copy()
+    # df_clone.replace(np.nan, 99999999, inplace=True)
+    # 找到 拐点出现的序号
+    inflexion_index = np.argmin(df_clone,axis=0)
+    val = df_clone.min(axis=0)
+    if inflexion_index < 4:
+        return 0
+    else:
+        return inflexion_index
+
+# 从拐点开始重新计算修正值
+def cal_inflexion_val(df,j):
+    df_clone = df.copy()
+    # df_clone.replace(0, 99999999, inplace=True)
+    # 获取拐点的值
+    inflexion_val =  df_clone.min(axis=0)
+
+    if j - find_inflexion(df) < 0 : # 说明 缺失值 在 拐点前面，需要从拐点开始递减
+        return inflexion_val - 1 * abs(j - find_inflexion(df))
+    else:
+        return inflexion_val + 1 * abs(j - find_inflexion(df))
+
+
