6a6850d107684b336f5181add5afab5ca78f170c
luoyujie2018 <46097778+luoyujie2018@users.noreply.github.com>
2019-01-16 15:26:42 +0800

    Update run.py

--- java/run.py
+++ java/run.py
@@ -1,16 +1,7 @@
 import sys
-from cdnn.algorithm.algorithm import *
-
-
-#方法选择
-def fun_choice(func,starttime,endtime):
-    parse_func = {
-        'diff': data_list(starttime,endtime)
-    }
-    parse_func[func]  # 执行相应方法
-
+from cdnn.algorithm.algorithm import data_list
 if __name__ ==  '__main__':
-    fun_choice(sys.argv[1],sys.argv[2],sys.argv[3])
+    data_list(sys.argv[1],sys.argv[2],sys.argv[3])
 
 
 
