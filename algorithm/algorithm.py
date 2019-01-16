327133a59c60c8135b48aea2389b2e9da48f76fd
luoyujie2018 <46097778+luoyujie2018@users.noreply.github.com>
2019-01-16 14:53:30 +0800

    四分法

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -78,23 +78,77 @@ def create_pyculiarity():
 
 ''' 四分法 测试'''
 
-def create_median():
-    data = get_data()
-    data['TIMESTAMP'] = pd.to_datetime(data['TIMESTAMP'])
-
-    dt = data['FP_TOTALENG']
-
+def create_median(field,data):
+    print(datetime.datetime.now())
+    data_copy = data.copy()
+    data['dt_time'] = pd.to_datetime(data['dt_time'])
+    data[['dt_val']]=data[['dt_val']].astype(float)
+    dt = data['dt_val']
     l = detectoutliers(data)
 
+    #处理异常数据l
+    # 1.找出0位置
+    data_zero=data[(data.dt_val == 0)]
+    # a > 1? "执行表达式1": "执行表达式2"
+    # 2.找出负值位置
+    data_minus=data[(data.dt_val < 0)]
+
+
+    #补充0后的数据 对比数据
+    errors_result=get_errors(data)
+    df_period = errors_result[0]
+    #3.找出data缺失值
+    data_miss=errors_result[1]
+    # data_miss=miss_result.dt_time.tolist()
+
+    # 4.极值
+    data_max=l
+    if not l.empty:
+        a = l.copy()
+        b=pd.concat([data_zero.copy(), data_minus.copy()], axis=0,
+                            ignore_index=True)
+        data_max=a.append(b).drop_duplicates(subset=['dt_time'], keep=False)
+    # 5.无异常数列
+
+    # 所有异常数据
+    error_all=pd.concat([data_zero, data_miss, data_minus, data_max], axis=0,
+                       ignore_index=True)
+
+    data_normal = df_period.copy().append(error_all).drop_duplicates(subset=['dt_time'], keep=False)
+    # 处理结果封装成标准字段
+    datas_normal=result(data_normal.dt_time.tolist(), data, df_period, 0)
+    datas_zero=result(data_zero.dt_time.tolist(), data, df_period, 2)
+    datas_miss=result(data_miss.dt_time.tolist(), data, df_period, 1)  # 缺失值
+    datas_minus=result(data_minus.dt_time.tolist(), data, df_period, 3)
+    # 极值处理
+    datas_max=result(data_max.dt_time.tolist(), data, df_period, 4)
+
+    # 汇总所有数据
+    list_all=pd.concat([datas_normal, datas_zero, datas_miss, datas_minus, datas_max], axis=0,
+                       ignore_index=True)
+    list_all['id']=field  # 添加id列
+    list_all=list_all.sort_values(by='dt_time', axis=0, ascending=True)  # 按时间排序
+    list_all.reset_index()
+    list_all['dt_time']=list_all['dt_time'].apply(lambda x: datetime.datetime.strftime(x, '%Y/%m/%d'))
+
+    list_all['dt_type']='median'
+    # 统一修正错误值
+    list_all=replace_data_lg(list_all.copy())
+
+    # 类型转换
+    list_all=list_all.astype('str')
+    data=data.astype('object')
+    oracleUtil("gxsy:gxsy123@120.26.116.232:1521/orcl", data_copy, 'data_in')
+    oracleUtil("gxsy:gxsy123@120.26.116.232:1521/orcl", list_all, 'data_out')
+    print(datetime.datetime.now())
+
     plt.title(u'median')
-    plt.plot(data['TIMESTAMP'], dt, label=u'first')
+    plt.plot(data['dt_time'], dt, label=u'first')
 
     # print(x,y)
-    plt.plot(l['TIMESTAMP'], l['FP_TOTALENG'], 'ro', label='check')
-
+    plt.plot(l['dt_time'], l['dt_time'], 'ro', label='check')
     plt.legend()
-
-    plt.show()
+    # plt.show()
 
 
 '''差分法 测试'''
@@ -325,6 +379,30 @@ def get_modes(l):
     obj=np.array(df).tolist();
     model=get_mode(obj)
     return model
+
+
+
+'''
+data:处理的数据
+list:处理后的结果 0位置为补充时间后的数据,1位置为缺失数据
+ '''
+def get_errors(data):
+    list=[]
+    # 3.缺失数据
+    # 补充了数据 ,这时补充的数据都是 0
+    data2=data.copy()
+    data2['dt_time']=data2['dt_time'].astype('datetime64')
+    df_period=data2.resample('D', on='dt_time').sum()
+    df_period=df_period.reset_index()
+    list.append(df_period)
+    # 求差集
+    a=df_period.copy();
+    b=data.copy()
+    b['dt_time']=b['dt_time'].astype('datetime64')
+    miss_result=a.append(b).drop_duplicates(subset=['dt_time'], keep=False)
+    data_miss=miss_result
+    list.append(data_miss)
+    return  list
 
     '''
     #把list整理成标准json   data为修正后的数据
